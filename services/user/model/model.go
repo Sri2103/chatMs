5337cefc8b69de62a429bbb36033cf72e7814461
Harsha <damasriharsha@gmail.com>
2025-01-09 09:12:10 -0500

    modified user model

--- services/user/model/model.go
+++ services/user/model/model.go
@@ -4,10 +4,16 @@ type UserModel struct {
 	UserId       string `json:"user_id"`
 	UserName     string `json:"user_name"`
 	Email        string `json:"email"`
-	passwordHash string `json:"password_hash"`
+	Role         Role   `json:"role"`
+	passwordHash string `json:"password_hash,omitempty"`
 }
 
 type Credentials struct {
 	UserName string `json:"user_name"`
 	password string `json:"password"`
 }
+
+type Role string
+
+const Admin Role = "admin"
+const Participant Role = "participant"
