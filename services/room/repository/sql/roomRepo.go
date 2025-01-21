85ca850157afbe2797dd512ccc966b9f2b5ae369
Harsha <damasriharsha@gmail.com>
2025-01-21 16:37:27 -0500

    add repo for details for room services

--- services/room/repository/sql/roomRepo.go
+++ services/room/repository/sql/roomRepo.go
@@ -25,13 +25,46 @@ func NewSqlRepo(cfg *config.Config) service.RoomRepository {
 }
 
 func (r *SqlRepo) SaveRoom(ctx context.Context, room *model.Room) (*model.Room, error) {
-	panic("not implemented") // TODO: Implement
+	_, err := r.Db.Exec(
+		"insert into room (room_id, room_name,created_by, users) values (?,?,?,?)",
+		room.RoomId, room.RoomName, room.CreatedBy, room.Users)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return room, nil
 }
 
 func (r *SqlRepo) FindById(ctx context.Context, roomId string) (*model.Room, error) {
-	panic("not implemented") // TODO: Implement
+	row := r.Db.QueryRow(
+		"select * from room where room_id=?",
+		roomId,
+	)
+	if row.Err() != nil {
+		return nil, row.Err()
+	}
+
+	var room model.Room
+	err := row.Scan(&room.RoomId, &room.RoomName, &room.CreatedBy, &room.Users)
+	if err != nil {
+		return nil, err
+	}
+
+	return &room, nil
+
 }
 
 func (r *SqlRepo) UpdateRoom(ctx context.Context, room *model.Room) error {
-	panic("not implemented") // TODO: Implement
+	_, err := r.Db.Exec(
+		"update room set created_by=?, users=?, room_name=? where room_id=?",
+		room.CreatedBy, room.Users, room.RoomName, room.RoomId,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
