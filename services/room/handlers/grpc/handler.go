76aa9995610ba59bfe1425d3dd36511c3effea50
Harsha <damasriharsha@gmail.com>
2025-01-21 15:44:05 -0500

    created handlers

--- services/room/handlers/grpc/handler.go
+++ services/room/handlers/grpc/handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/sri2103/chat_me/protos/room"
+	"github.com/sri2103/chat_me/services/room/model"
 	"github.com/sri2103/chat_me/services/room/service"
 )
 
@@ -18,18 +19,51 @@ func NewGrpcHandler(svc service.Service) room.RoomServiceServer {
 	}
 }
 
-func (h *Handler) CreateRoom(_ context.Context, _ *room.CreateRoomRequest) (*room.CreateRoomResponse, error) {
-	panic("not implemented") // TODO: Implement
+func (h *Handler) CreateRoom(ctx context.Context, req *room.CreateRoomRequest) (*room.CreateRoomResponse, error) {
+	rm := &model.Room{
+		RoomName:  req.RoomName,
+		CreatedBy: req.CreatedBy,
+		Users:     req.GetUserIds(),
+	}
+	rd, err := h.service.CreateRoom(ctx, rm)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var res room.CreateRoomResponse
+	res.Success = true
+	res.RoomId = rd.RoomId
+	return &res, nil
 }
 
-func (h *Handler) GetRoomDetails(_ context.Context, _ *room.GetRoomRequest) (*room.GetRoomResponse, error) {
-	panic("not implemented") // TODO: Implement
+func (h *Handler) GetRoomDetails(ctx context.Context, req *room.GetRoomRequest) (*room.GetRoomResponse, error) {
+	rId := req.RoomId
+	rm, err := h.service.GetRoomDetails(ctx, rId)
+	if err != nil {
+		return nil, err
+	}
+	var res room.GetRoomResponse
+	res.RoomId = rm.RoomId
+	res.RoomName = rm.RoomName
+	res.CreatedBy = rm.CreatedBy
+	res.UserIds = rm.Users
+	return &res, nil
 }
 
-func (h *Handler) UpdateRoomSettings(_ context.Context, _ *room.UpdateRoomRequest) (*room.UpdateRoomResponse, error) {
-	panic("not implemented") // TODO: Implement
-}
+func (h *Handler) UpdateRoomSettings(ctx context.Context, req *room.UpdateRoomRequest) (*room.UpdateRoomResponse, error) {
+	rm := &model.Room{
+		RoomName: req.RoomName,
+		Users:    req.GetUserIds(),
+	}
 
-func (h *Handler) mustEmbedUnimplementedRoomServiceServer() {
-	panic("not implemented") // TODO: Implement
+	err := h.service.UpdateRoom(ctx, rm)
+
+	if err != nil {
+		return nil, err
+	}
+	var res room.UpdateRoomResponse
+	res.Success = true
+	return &res, nil
+
 }
