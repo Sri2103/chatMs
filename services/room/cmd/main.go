b6ef421eb26477cfbc74236ffc615849ac9bf299
Harsha <damasriharsha@gmail.com>
2025-01-21 20:22:14 -0500

    add the main code for  room service

--- services/room/cmd/main.go
+++ services/room/cmd/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/sri2103/chat_me/protos/room"
+	"github.com/sri2103/chat_me/services/room/config"
+	handler "github.com/sri2103/chat_me/services/room/handlers/grpc"
+	sqlRepo "github.com/sri2103/chat_me/services/room/repository/sql"
+	"github.com/sri2103/chat_me/services/room/service"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	fmt.Println("starting room service")
+	port := flag.Int("port", 8082, "port for the room service")
+	sqlitePath := flag.String("sqlite", "", "sqlite database path")
+	env := flag.String("env", "dev", "environment for the service")
+	flag.Parse()
+	cfg := &config.Config{
+		Port:        *port,
+		SqlitePath:  *sqlitePath,
+		Environment: *env,
+	}
+	err := SetupGRPCServer(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func SetupGRPCServer(cfg *config.Config) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		return err
+	}
+
+	repo := sqlRepo.NewSqlRepo(cfg)
+	svc := service.New(repo)
+	s := grpc.NewServer()
+	h := handler.NewGrpcHandler(svc)
+	room.RegisterRoomServiceServer(s, h)
+	err = s.Serve(lis)
+	if err != nil {
+		return err
+	}
+	return nil
 }
