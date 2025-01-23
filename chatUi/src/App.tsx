084aa87a14a64548376c17fb8a5c047191a9853c
Harsha <damasriharsha@gmail.com>
2025-01-23 08:28:19 -0500

    add register and login page

--- chatUi/src/App.tsx
+++ chatUi/src/App.tsx
@@ -1,14 +1,29 @@
-import ChatLayout from "./pages/ChatLayout"
-function App() {
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import LoginRegister from './pages/LoginRegister';
 
+const Home = () => {
   return (
-    <>
-      <div className="bg-gray-100 h-screen flex flex-col">
-        <ChatLayout />
-      </div>
-    </>
-  )
+    <div className="bg-gray-100 h-screen flex justify-center items-center">
+      <h1>Welcome to the Home Page</h1>
+    </div>
+  );
+};
 
-}
 
-export default App
+
+
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/authenticate" element={<LoginRegister />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
+
